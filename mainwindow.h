32c2174d69b5e9a2240757a138c30782f17ff6c7
jackstraw <1107247946@qq.com>
2018-04-09 02:11:18 +0800

    release v1.0.

--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,32 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_actionOpen_triggered();
+
+    void on_actionNew_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionRedo_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionAbout_mNotepad_triggered();
+
+    void on_actionQuit_triggered();
+
+    void on_actionSave_as_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QString file_path_;
 };
 
 #endif // MAINWINDOW_H
